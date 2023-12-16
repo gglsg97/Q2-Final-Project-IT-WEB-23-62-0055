7a338cd9c909a290de04ba520fa3396723f12983
gglsg97 <121725561+gglsg97@users.noreply.github.com>
2023-12-16 22:26:30 +0530

    Q2 Answers Updated

---
+++ Q2 iv) Queries with Answers/All SQL Queries.sql
@@ -0,0 +1,16 @@
+﻿USE [StudentsInformationContext-0fdee002-61a2-40d2-a343-173121048fa5];
+
+SELECT * FROM Student;
+
+SELECT StudentId,Name,City 
+FROM Student 
+WHERE City='Kandy'; 
+
+UPDATE Student 
+SET City='Galle' 
+WHERE StudentId=4;
+
+SELECT S.StudentId,S.Name,S.City,S.CourseID,C.Name 
+FROM Student AS S 
+LEFT JOIN Course AS C 
+ON S.CourseID = C.CourseID;
